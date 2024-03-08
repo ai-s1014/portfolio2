743fa18f66136eb735169e1941f93a24850479b2
ai-s1014 <aaaip37@gmail.com>
2024-03-09 00:26:32 +0900

    “PC表示swiper”

--- js/script.js
+++ js/script.js
@@ -7,21 +7,28 @@ $('.drawer-icon').on('click', function() {
 });
 
 // header スクロール 表示・非表示
-const header = document.querySelector('.header'); //.headerを取得
-let prevY = window.scrollY; // 現在のスクロール位置を取得
+// ウィンドウの幅が640px以下かどうかをチェック
+if (window.innerWidth <= 641) {
+  // 640px以下の画面のスクロール動作
+  const header = document.querySelector('.header');
+  let prevY = window.scrollY;
 
-window.addEventListener('scroll', () => {
-  const currentY = window.scrollY;
-  // 現在のスクロール位置が前回よりも上にある場合と下にある場合で処理を分岐
-  if (currentY < prevY) { // 上にスクロールしてる場合
-    header.classList.remove('hidden');
-  } else {
-  if (currentY > 150) { // 下にスクロールしてる場合 高さ150px
-      header.classList.add('hidden');
-    }
-  }
-  prevY = currentY;
-});
+  window.addEventListener('scroll', () => {
+      const currentY = window.scrollY;
+
+      // 上にスクロールしている場合、ヘッダーを表示
+      if (currentY < prevY) {
+          header.classList.remove('hidden');
+      } else {
+          // 下にスクロールしていて、150pxを超えた場合、ヘッダーを非表示
+          if (currentY > 150) {
+              header.classList.add('hidden');
+          }
+      }
+
+      prevY = currentY;
+  });
+}
 
 // swiper hero
 const swiper = new Swiper('.swiper', {
